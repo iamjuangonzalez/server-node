5a5f6cb887815c40b7d1346008415070d64f3d88
iamjuangonzalez <juangonzalez1@unisangil.edu.co>
2022-11-13 18:14:54 -0500

    config dbprueba

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const dbConnect = async () => {
   /* const DB_URI =
     "mongodb+srv://medical365:R3dPrvOwaOMfrGOB@cluster0.953zg4c.mongodb.net/test"; */
-  const DB_URI = "mongodb://localhost:27017/dbPrueba";
+  const DB_URI =
+    "mongodb+srv://medical365:R3dPrvOwaOMfrGOB@cluster0.953zg4c.mongodb.net/dbPrueba";
+  /* const DB_URI = "mongodb://localhost:27017/dbPrueba"; */
   await mongoose.connect(
     DB_URI,
     {
